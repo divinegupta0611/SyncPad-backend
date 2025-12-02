18b6a5cce341418dbbc83eaaf1128f016fc42778
divine <guptadivine0611@gmail.com>
2025-12-02 20:45:35 +0530

    CORS fixed

--- index.js
+++ index.js
@@ -498,7 +498,7 @@ setInterval(() => {
   }
 }, 60 * 60 * 1000);
 // Change all localhost to frontend deployed link
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 'https://syncpad-backend.onrender.com';
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
   console.log(`WebSocket server ready for connections`);
